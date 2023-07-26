6201ae5028435741a514097c68ed2be9f31c8761
martins-o <jojololamartins686@gmail.com>
2023-07-26 07:29:00 +0100

    Security complete

--- src/main/java/furniture/ecommerce/furnitureecommerce/data/dto/request/RegisterRequest.java
+++ src/main/java/furniture/ecommerce/furnitureecommerce/data/dto/request/RegisterRequest.java
@@ -2,6 +2,7 @@ package furniture.ecommerce.furnitureecommerce.data.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,12 +21,14 @@ public class RegisterRequest {
 	@NotNull(message = "Last name address is required")
 	private String lastName;
 	@NotNull(message = "Email address is required")
-	@Email(regexp = EMAIL_REGEX_STRING)
+	@Email(regexp = EMAIL_REGEX_STRING, message = "please enter a valid email address")
 	private String email;
 	@Size(min = 6, max = 20)
 	@NotNull(message = "Password address is required")
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@#$%^&+=])[A-Za-z\\d@#$%^&+=]{8,}$", message = "Enter a valid password")
 	private String password;
 	@NotNull(message = "Confirm Password address is required")
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@#$%^&+=])[A-Za-z\\d@#$%^&+=]{8,}$", message = "Enter a valid password")
 	private String Confirmpassword;
 	@NotNull(message = "Phone Number address is required")
 	private String phoneNumber;
